658dd75c1cc37403cedc474e470b48b929e7c00a
Noah A <nanderson815@gmail.com>
2019-04-03 17:56:47 -0400

    landing page

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -4,12 +4,19 @@ import './Landing.css';
 class Landing extends React.Component {
 
     render() {
-        return ( 
+        return (
             <div>
                 <header>
-
+                    <div className="container">
+                        <div className="row">
+                            <div className="col s5 offset-s7">
+                                <h3 className="white-text">How Retailers Handle Cash</h3>
+                                <h5 className="white-text">Instantly make cash deposits</h5>
+                            </div>
+                        </div>
+                    </div>
                 </header>
-            </div>  
+            </div >
         );
     }
 }
