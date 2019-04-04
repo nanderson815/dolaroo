99f375a620bef521c7026bd7cbfe52df370f0829
Noah A <nanderson815@gmail.com>
2019-04-04 07:40:42 -0400

    added footer

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -80,7 +80,7 @@ class Landing extends React.Component {
                                 <div className="row">
                                     <div className="textbox col m5">
                                         <h5 className="white-text">Sign Up Requirements</h5>
-                                        <p className="white-text">An in-person consultation and product demo are the first step of the sign up process. 
+                                        <p className="white-text">An in-person consultation and product demo are the first step of the sign up process.
                                         Sign up today to be up and running in as little as two weeks.</p>
                                         <div className="center-align">
                                             <a className="waves-effect waves-light btn  blue darken-4">Sign Up Now</a>
@@ -94,6 +94,32 @@ class Landing extends React.Component {
                         </div>
                     </div>
                 </section>
+
+                <footer class="page-footer blue darken-4">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col l6 s12">
+                                <h5 class="white-text">Footer Content</h5>
+                                <p class="grey-text text-lighten-4">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
+                            </div>
+                            <div class="col l4 offset-l2 s12">
+                                <h5 class="white-text">Links</h5>
+                                <ul>
+                                    <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                                    <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                                    <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                                    <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footer-copyright">
+                        <div class="container">
+                            © 2019 Copyright Text
+                        </div>
+                    </div>
+                </footer>
 
             </div >
         );
