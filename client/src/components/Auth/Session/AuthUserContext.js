e2e5feeed1db3ff5a8723cd11f2bf3d24238e404
plinck <paul.linck@gmail.com>
2019-04-05 17:54:48 -0400

    Fixed nav link and created HOC for AuthUserContext

--- client/src/components/Auth/Session/AuthUserContext.js
+++ client/src/components/Auth/Session/AuthUserContext.js
@@ -2,4 +2,11 @@ import React from 'react';
 
 const AuthUserContext = React.createContext(null);
 
+const withAuthUserContext = Component => props => (
+  <AuthUserContext.Consumer>
+    {authUser => <Component {...props} authUser={authUser} />}
+  </AuthUserContext.Consumer>
+);
+
 export default AuthUserContext;
+export { withAuthUserContext };
