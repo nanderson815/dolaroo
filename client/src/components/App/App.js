711ad5cc915b11e8ef3a9372c67580535bb81ff0
Noah A <nanderson815@gmail.com>
2019-04-10 22:18:56 -0400

    adding form for payment

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -10,6 +10,7 @@ import HomePage from '../Dashboard';
 import AccountPage from '../Account/Account';
 import Payment from '../Payment/Payment';
 
+
 // Auth components
 import SignUpForm from '../Auth/SignUp/SignUp';
 import SignInForm from '../Auth/SignIn/SignIn';
@@ -21,18 +22,18 @@ import provideAuthUserContext from '../Auth/Session/provideAuthUserContext';
 class App extends React.Component {
   render() {
     return (
-        <Router>
-          <div>
-            <Navigation />
-            <Route exact path="/" component={LandingPage} />
-            <Route exact path="/signup" component={SignUpForm} />
-            <Route exact path="/signin" component={SignInForm} />
-            <Route exact path="/pw-forget" component={PasswordForgetPage} />
-            <Route exact path="/dashboard" component={HomePage} />
-            <Route exact path="/account" component={AccountPage} />
-            <Route exact path="/payment" component={Payment} />
-          </div>
-        </Router>
+      <Router>
+        <div>
+          <Navigation />
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/signup" component={SignUpForm} />
+          <Route exact path="/signin" component={SignInForm} />
+          <Route exact path="/pw-forget" component={PasswordForgetPage} />
+          <Route exact path="/dashboard" component={HomePage} />
+          <Route exact path="/account" component={AccountPage} />
+          <Route exact path="/dashboard/payment" component={Payment} />
+        </div>
+      </Router>
     );
   }
 }
--- client/src/components/Payment/Payment.js
+++ client/src/components/Payment/Payment.js
@@ -1,13 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
+import DepositDB from '../Dashboard/Deposit/DepositDB';
 
-const Payment = (props) => {
-    return (
-        <div>
-            <div className="container">
-                <h1>Payment Page</h1>
+
+
+class Payment extends Component {
+    state = {
+        credit: 0,
+        minimum: 25,
+        other: 0
+    }
+
+    componentDidMount() {
+
+        DepositDB.get("credit")
+            .then(res => this.setState({ credit: res[0].balance }));
+
+    }
+
+
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col s12">
+                            <div className="card">
+                                <div className="card-content">
+                                    <span className="card-title"><h4>Make a payment</h4></span>
+
+                                    <form action="#!">
+                                        <p>
+                                            <label>
+                                                <input className="with-gap" name="group1" type="radio" value={this.state.credit} />
+                                                <span>Current Balance: ${this.state.credit}</span>
+                                            </label>
+                                        </p>
+                                        <p>
+                                            <label>
+                                                <input className="with-gap" name="group1" type="radio" value={this.state.minimum} />
+                                                <span>Minimum Payment: ${this.state.minimum}</span>
+                                            </label>
+                                        </p>
+                                        <p>
+                                            <label>
+                                                <input className="with-gap" name="group1" type="radio" />
+                                                <span> Other: ${this.state.other} 
+                                                    <span className="input-field">
+                                                        <input id="last_name" type="text" className="validate" value={this.state.other}/>
+                                                    </span>
+                                                </span>
+                                            </label>
+                                        </p>
+                                    </form>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+
 
 export default Payment;
