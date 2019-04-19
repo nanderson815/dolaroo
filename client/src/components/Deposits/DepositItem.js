fbf0eb7824d3a222d0b2e36044670aacf454ef29
Paul Linck <paul.linck@gmail.com>
2019-04-18 21:11:45 -0400

    Expansion Panels

--- client/src/components/Deposits/DepositItem.js
+++ client/src/components/Deposits/DepositItem.js
@@ -1,34 +1,101 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
-import { withAuthUserContext } from '../Auth/Session/AuthUserContext';
+import {withRouter} from 'react-router-dom';
+import {withAuthUserContext} from '../Auth/Session/AuthUserContext';
 
 import moment from "moment";
 
-const DepositItem = (props) => {
+// New explansion panels
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const SimpleExpansionPanel = (props) => {
     // decontruct props
-    let { uid, amount, ones, fives, tens, twenties, fifties, hundreds, time, user } =  props.deposit;
+    const {
+        amount,
+        ones,
+        fives,
+        tens,
+        twenties,
+        fifties,
+        hundreds,
+        time,
+        email
+    } = props.deposit;
+    let jsDate = new Date(time);
+    const dateTime = moment(jsDate).format("YYYY-MM-DD HH:mm:ss");
+    
+    return (
+        <ExpansionPanel>
+            <ExpansionPanelSummary className="row" expandIcon={< ExpandMoreIcon />}>
+                <i className="material-icons green-text col s1 m1">attach_money</i>
+                <Typography className="col s5 m3">{dateTime}</Typography>
+                <Typography className="col s6 m3">{email}</Typography>
+                <Typography 
+                    className="col s12 m2 offset-m3">
+                    ${amount.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                </Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+                <Typography className="row grey-text">
+                    <blockquote>
+                        Ones: ${ones * 1}, Fives: ${fives * 5}, Tens: ${tens * 10}, Twentys: ${twenties * 20}, Fifties: ${fifties * 50}, Hundreds: ${hundreds * 100}
+                    </blockquote>
+                </Typography>
+            </ExpansionPanelDetails>
+        </ExpansionPanel>
+    );
+};
+
+const materialCollection = (props) => {
+    // decontruct props
+    let {
+        amount,
+        ones,
+        fives,
+        tens,
+        twenties,
+        fifties,
+        hundreds,
+        time,
+        email
+    } = props.deposit;
     let jsDate = new Date(time);
     const dateTime = moment(jsDate).format("YYYY-MM-DD HH:mm:ss");
 
-    return ( 
+    return (
         <li className="collection-item avatar z-depth-2">
             <i className="material-icons circle green">attach_money</i>
             <h5 className="title row">
-                <div className ="col s6 m3">{dateTime}</div>
-                <div className ="col s6 m3">{user}</div>
-                <div className ="col s12 m3 offset-m3">${amount.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</div>
+                <div className="col s6 m3">{dateTime}</div>
+                <div className="col s6 m3">{email}</div>
+                <div className="col s12 m3 offset-m3">${amount
+                        .toFixed(2)
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</div>
             </h5>
             <div className="row grey-text">
-                <blockquote>Ones: ${ones*1},
-                Fives: ${fives*5},
-                Tens: ${tens*10},
-                Twentys: ${twenties*20},
-                Fifties: ${fifties*50},
-                Hundreds: ${hundreds*100}
+                <blockquote>Ones: ${ones * 1}, Fives: ${fives * 5}, Tens: ${tens * 10}, Twentys: ${twenties * 20}, Fifties: ${fifties * 50}, Hundreds: ${hundreds * 100}
                 </blockquote>
             </div>
-            <a href="#!" className="secondary-content"><i className="material-icons">grade</i></a>
+            <a href="#!" className="secondary-content">
+                <i className="material-icons">grade</i>
+            </a>
         </li>
+    );
+};
+
+const DepositItem = (props) => {
+    if (false) {
+        return (
+            materialCollection(props)
+        );
+    }    
+
+    return (
+        SimpleExpansionPanel(props)
     );
 };
 
