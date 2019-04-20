bd5b208fd03811366200bf7445ea6ce7aead03f8
Noah A <nanderson815@gmail.com>
2019-04-20 13:21:52 -0400

    adding savings card

---
+++ client/src/components/Dashboard/Savings/Savings.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Savings = (props) => {
+    return (
+        <div>
+            <div className="col s12 l6">
+                <div className="card">
+                    <div className="card-content pCard">
+                        <span className="card-title">Savings</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Savings;
