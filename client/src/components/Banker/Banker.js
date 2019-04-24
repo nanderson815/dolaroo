b06f67b98cf0200e8c0749ae93773a0b445a6fe7
Paul Linck <paul.linck@gmail.com>
2019-04-23 21:07:35 -0400

    updated deposits using batch

--- client/src/components/Banker/Banker.js
+++ client/src/components/Banker/Banker.js
@@ -4,12 +4,27 @@ import { Redirect } from 'react-router';
 import Prospects from "./Prospects";
 import { withAuthUserContext } from "../Auth/Session/AuthUserContext";
 
+import SettledDepositsDB from "./SettledDepositsDB";
+
 class Banker extends React.Component {
+
+    updateDeposits = () => {
+        // Add the proper boolean to the depoists table for all docs
+        SettledDepositsDB.setDepositsToCurrent().then(res => {
+            alert(res);
+        }).catch(err => {
+            console.error(`error updating ${err}`);
+        });
+    }
   
     render() {
         if (this.props.user && this.props.user.isBanker) {
             return ( 
                 <div className="container">
+                    <div className="row center-align">
+                        <br />
+                        <button className="btn center-align blue darken-4" onClick={this.updateDeposits}>Batch Update Deposits</button>{" "}
+                    </div>
                     <h5 className="center-align">Prospects</h5>
                     <Prospects />
                 </div>
