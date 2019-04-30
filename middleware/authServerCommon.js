68db457bb292417469be59ccb1dc62b54ee29f8a
plinck <paul.linck@gmail.com>
2019-04-30 10:01:06 -0400

    Heroku stuff

--- middleware/authServerCommon.js
+++ middleware/authServerCommon.js
@@ -17,6 +17,24 @@ if (process.env.GOOGLE_APPLICATION_CREDENTIALS) {
         databaseURL: databaseURL,
         storageBucket: storageBucket
     });
+} else if (process.env.HEROKU_GOOGLE_DEPLOYMENT) {
+    const serviceAccount = {
+        "type": "service_account",
+        "project_id": "project3-noahpauljj-fintech2",
+        "private_key_id": process.env.GOOGLE_PRIVATE_KEY_ID,
+        "private_key":  process.env.GOOGLE_PRIVATE_KEY,
+        "client_email": "firebase-adminsdk-f363h@project3-noahpauljj-fintech2.iam.gserviceaccount.com",
+        "client_id": "104612084028429486586",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-f363h%40project3-noahpauljj-fintech2.iam.gserviceaccount.com"
+    }
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: databaseURL,
+        storageBucket: storageBucket
+    });
 } else {
     admin.initializeApp({
         databaseURL: databaseURL,
