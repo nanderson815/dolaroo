e69f72aa4b10ec058c6cd720dc501b53ddafb5b7
Noah A <nanderson815@gmail.com>
2019-04-30 12:55:56 -0400

    small change to PC component

--- client/src/components/Dashboard/ProvisionalCredit/provisionalCredit.js
+++ client/src/components/Dashboard/ProvisionalCredit/provisionalCredit.js
@@ -9,7 +9,7 @@ const ProvisionalCredit = (props) => {
     var time = new Date(date.getTime());
     time.setMonth(date.getMonth() + 1);
     time.setDate(0);
-    var days = time.getDate() > date.getDate() ? time.getDate() - date.getDate() : 0;
+    var days = time.getDate() > date.getDate() ? time.getDate() - date.getDate() : "Bill Due Today";
 
 
     return (
