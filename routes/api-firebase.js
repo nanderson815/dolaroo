d22cfefb02320f411af3c8931942bbe1c81c0c90
Noah A <nanderson815@gmail.com>
2019-05-02 18:56:43 -0400

    adding more routes

--- routes/api-firebase.js
+++ routes/api-firebase.js
@@ -1,24 +1,62 @@
 const admin = require("../middleware/authServerCommon");
 const db = admin.firestore();;
 
+let deposits = [];
+let depositsArchive = [];
+let cash = [];
+let credit = [];
 
+// Get all deposits on load
+db.collection("deposits").onSnapshot((querySnapshot) => {
+    querySnapshot.forEach(doc => {
+        let deposit = {};
+        deposit = doc.data();
+        deposit.id = doc.id;
+        deposits.push(deposit);
+    });
+});
+
+// Get all archived deposits on load
+db.collection("depositsarchive").onSnapshot((querySnapshot) => {
+    querySnapshot.forEach(doc => {
+        let deposit = {};
+        deposit = doc.data();
+        deposit.id = doc.id;
+        depositsArchive.push(deposit);
+    });
+});
+
+// Get balances over time
+db.collection("cash").onSnapshot(querySnapshot => {
+    querySnapshot.forEach(doc => {
+        let total = {};
+        total = doc.data();
+        total.id = doc.id;
+        cash.push(total);
+    });
+});
+
+// Get credit over time
+db.collection("credit").onSnapshot(querySnapshot => {
+    querySnapshot.forEach(doc => {
+        let total = {};
+        total = doc.data();
+        total.id = doc.id;
+        credit.push(total);
+    });
+});
 
 module.exports = function (app) {
     app.get("/api/firestore/deposits", (req, res) => {
-        console.log("hit");
-        db.collection("deposits").get().then((querySnapshot) => {
-            let deposits = [];
-            querySnapshot.forEach((doc) => {
-                querySnapshot.forEach(doc => {
-                    let deposit = {};
-                    deposit = doc.data();
-                    deposit.id = doc.id;
-                    deposits.push(deposit);
-                });
-                return (deposits);
-            });
-            res.json(deposits);
-        });
+        res.json(deposits);
     });
+
+    app.get("/api/firestore/depositsArchive", (req, res) => {
+        res.json(depositsArchive);
+    });
+
+    app.get("/api/firestore/cash", (req, res) => {
+        res.json(cash);
+    })
 };
 
