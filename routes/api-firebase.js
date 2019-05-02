5b4ceb8a7fce4d4b488a31dd9ad609d0f3652539
Noah A <nanderson815@gmail.com>
2019-05-02 19:23:34 -0400

    adding more routes

--- routes/api-firebase.js
+++ routes/api-firebase.js
@@ -14,7 +14,7 @@ db.collection("deposits").onSnapshot((querySnapshot) => {
         deposit.id = doc.id;
         deposits.push(deposit);
     });
-});
+}, (err) => console.log(err));
 
 // Get all archived deposits on load
 db.collection("depositsarchive").onSnapshot((querySnapshot) => {
@@ -24,7 +24,7 @@ db.collection("depositsarchive").onSnapshot((querySnapshot) => {
         deposit.id = doc.id;
         depositsArchive.push(deposit);
     });
-});
+}, (err) => console.log(err));
 
 // Get balances over time
 db.collection("cash").onSnapshot(querySnapshot => {
@@ -34,7 +34,7 @@ db.collection("cash").onSnapshot(querySnapshot => {
         total.id = doc.id;
         cash.push(total);
     });
-});
+}, (err) => console.log(err));
 
 // Get credit over time
 db.collection("credit").onSnapshot(querySnapshot => {
@@ -44,19 +44,36 @@ db.collection("credit").onSnapshot(querySnapshot => {
         total.id = doc.id;
         credit.push(total);
     });
-});
+}, (err) => console.log(err));
 
+
+// Routes Below ------------------------------------------------------------------------------------------
 module.exports = function (app) {
+    // Send all deposits
     app.get("/api/firestore/deposits", (req, res) => {
         res.json(deposits);
     });
-
+    // Send all archived deposits
     app.get("/api/firestore/depositsArchive", (req, res) => {
         res.json(depositsArchive);
     });
-
+    // Send all cash
     app.get("/api/firestore/cash", (req, res) => {
         res.json(cash);
+    });
+    // Send all credit
+    app.get("/api/firestore/cash", (req, res) => {
+        res.json(credit);
+    })
+    // Get pending deposit total - ie not in safe, but not settled
+    app.get("/api/firestore/getPendingTotal", (req, res) => {
+        let result = new Promise((resolve, reject) => {
+            let total = 0;
+            let pendingTrans = depositsArchive.filter(dep => dep.awaitingSettlement === true);
+            pendingTrans.forEach(tran => total += tran.amount);
+            resolve(total);
+        });
+        result.then(result => res.json(result));
     })
 };
 
