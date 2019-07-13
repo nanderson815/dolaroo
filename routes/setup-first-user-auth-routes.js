c80fb31dddd1a7818d3f88ca9e5b6d594e30ed65
Noah A <nanderson815@gmail.com>
2019-07-12 20:23:59 -0400

    added simple text password to create first user

--- routes/setup-first-user-auth-routes.js
+++ routes/setup-first-user-auth-routes.js
@@ -4,12 +4,26 @@ var admin = require('firebase-admin');
 
 module.exports = function (app) {
 
-    app.post("/setFirstUser/setCustomClaims/:uid", (req, res) => {
+    app.post("/setFirstUser/setCustomClaims/:uid/:pass", (req, res) => {
         let uid = req.params.uid
+        let password = req. params.pass
 
-        admin.auth.setCustomUserClaims(uid, { testCompany: true }).then(() => {
-            res.json(uid)
-        })
+        // check if user has submitted a password as request param. 
+        if (password === "testPassword"){
+            let firstUserClaims = {
+                testCompany: true,
+                isAdmin: true,
+                isCashier: false,
+                isUser: false
+            }
+            // admin.auth.setCustomUserClaims(uid, firstUserClaims).then(() => {
+            //     res.json(uid)
+            // })
+
+            res.json(firstUserClaims);
+        }
+
+        
 
     });
 
