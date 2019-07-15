90e4185bb6ace05540fd9f48e7836020066b4bb2
Noah A <nanderson815@gmail.com>
2019-07-14 21:14:31 -0400

    pushing

--- routes/setup-first-user-auth-routes.js
+++ routes/setup-first-user-auth-routes.js
@@ -6,29 +6,21 @@ module.exports = function (app) {
 
     app.post("/setFirstUser/setCustomClaims/:uid/:pass", (req, res) => {
         let uid = req.params.uid
-        let password = req. params.pass
+        let password = req.params.pass
 
         // check if user has submitted a password as request param. 
-        if (password === "testPassword"){
+        if (password === "testPassword") {
             let firstUserClaims = {
                 testCompany: true,
                 isAdmin: true,
                 isCashier: false,
                 isUser: false
             }
-            // admin.auth.setCustomUserClaims(uid, firstUserClaims).then(() => {
-            //     res.json(uid)
-            // })
+            admin.auth.setCustomUserClaims(uid, firstUserClaims).then(() => {
+                res.json(uid)
+            })
 
             res.json(firstUserClaims);
         }
-
-        
-
     });
-
-    app.get("/test/test", (req, res) => {
-        res.json("hello.")
-    });
-
 }
