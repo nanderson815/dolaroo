6cfadcaea349725e3f148e71b4c12a17b874169c
Noah A <nanderson815@gmail.com>
2019-07-16 18:13:21 -0400

    creating first user

--- routes/setup-first-user-auth-routes.js
+++ routes/setup-first-user-auth-routes.js
@@ -4,21 +4,23 @@ var admin = require('firebase-admin');
 
 module.exports = function (app) {
 
-    app.post("/setFirstUser/setCustomClaims/:uid/:pass", (req, res) => {
+    app.post("/setFirstUser/setCustomClaims/:uid/:loc/:pass", (req, res) => {
         let uid = req.params.uid
+        let loc = req.params.loc
         let password = req.params.pass
 
         // check if user has submitted a password as request param. 
         if (password === "testPassword") {
             let firstUserClaims = {
                 testCompany: true,
+                [loc]: true,
                 isAdmin: true,
                 isCashier: false,
                 isUser: false
             }
-            admin.auth.setCustomUserClaims(uid, firstUserClaims).then(() => {
-                res.json(uid)
-            })
+            // admin.auth.setCustomUserClaims(uid, firstUserClaims).then(() => {
+            //     res.json(uid)
+            // })
 
             res.json(firstUserClaims);
         }
