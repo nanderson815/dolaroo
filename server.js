fa6a994589334d9a677224e6bc490e28cb85128d
Noah A <nanderson815@gmail.com>
2019-10-14 17:21:12 -0400

    changed server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 require("dotenv").config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 const mongoose = require("mongoose");
 
